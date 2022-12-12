985f272f9c701a279f800969256a1d6fad5449f5
sotblad <sotblad@monetaryunit.org>
2022-12-12 08:47:40 +0200

    3.2 finished

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -4,6 +4,7 @@ window.onload = function() {
     var usrContent = $('div.well.col-sm-4')
     var firstImg;
     var firstCaption;
+    var firstImgHref;
     var uid = -1;
 
     usrContent.hover(function () {
@@ -22,6 +23,7 @@ window.onload = function() {
             uid = -1
             firstImg = null;
             firstCaption = null;
+            firstImgHref = null;
         }
         if (!firstImg) {
             firstImg = $(this).find("img#firstimage").attr("src");
@@ -32,11 +34,16 @@ window.onload = function() {
             firstCaption = $(this).find("h2#firstcaption").attr("caption");
         }
 
+        if (!firstImgHref) {
+            firstImgHref = $(this).find("img#firstimage").attr("href");
+        }
+
         var slides = $(this).find("div#slides").find("img");
         var counterPlace = $(this).find("h2#counter");
         counter = counterPlace.attr("counter");
         theInterval = setInterval(function () {
             image.attr("src", $(slides[counter]).attr("src"));
+            image.attr("href", $(slides[counter]).attr("href"));
             caption.text($(slides[counter]).attr("alt"));
 
             counter = parseInt(counter)+1;
@@ -44,6 +51,7 @@ window.onload = function() {
 
             if(counter == slides.length+1) {
                 image.attr("src", firstImg);
+                image.attr("href", firstImgHref);
                 caption.text(firstCaption);
 
                 counter = 0;
